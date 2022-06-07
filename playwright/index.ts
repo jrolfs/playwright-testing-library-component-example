3fef0f1b88f5dff46fe9e3770eb760e41b9cc9ac
Jamie Rolfs <jamie.rolfs@gmail.com>
2022-06-06 18:57:05 -0700

    docs: add comments to workaround stuff

--- playwright/index.ts
+++ playwright/index.ts
@@ -1,5 +1,10 @@
 import * as TestingLibraryDom from '@testing-library/dom';
 
+/**
+ * Copy pasta'ed from `playwright-testing-library`
+ *
+ * @see {@link https://github.com/jrolfs/playwright-testing-library-component-example/blob/43d8d56db9cdc6606e8ddd9a0b3cf18daf4f12b6/playwright/index.ts#L3-L8}
+ */
 const reviver = (_: string, value: string) => {
   if (value.toString().includes('__REGEXP ')) {
     const match = /\/(.*)\/(.*)?/.exec(value.split('__REGEXP ')[1]);
@@ -10,6 +15,12 @@ const reviver = (_: string, value: string) => {
   return value;
 };
 
+/**
+ * Work around issue with Playwright Testing Library + Experimental Component
+ * support in Playwright Test by loading the global JavaScript we need here.
+ *
+ * @see {@link https://github.com/testing-library/playwright-testing-library/blob/19c9ebd37917ec04eedf02e6dbe8d55b45f0855b/lib/fixture/locator.ts#L120-L124}
+ */
 const attachTestingLibrary = () => {
   window.TestingLibraryDom = TestingLibraryDom;
   window.__testingLibraryReviver = reviver;
